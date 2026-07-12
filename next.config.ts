07c1bf97e822b0cd64dfd3fa404c15cb69f14bf7
nathee-rua <nathee.rua@thaimooc.ac.th>
2026-07-12 13:44:56 +0700

    chore: resolve deprecated eslint key in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: any = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
